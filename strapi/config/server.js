87bbd322f4c56758fcc2d6cf2e2b6a77cd6e30b5
Eric Ahmann <eric@ahmann.me>
2021-10-29 23:13:26 -0500

    Some changes....

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      secret: env('ADMIN_JWT_SECRET', 'e49ba534ad9193da1142ca0438b6b957'),
     },
   },
   vercel: {
