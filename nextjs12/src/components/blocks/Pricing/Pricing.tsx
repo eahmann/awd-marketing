5d59b97fb9e0b06a035158708d8ddb0dd0853ab5
Eric Ahmann <eric@ahmann.me>
2021-11-04 08:39:31 -0500

    Fix imports

--- nextjs12/src/components/blocks/Pricing/Pricing.tsx
+++ nextjs12/src/components/blocks/Pricing/Pricing.tsx
@@ -1,66 +1,13 @@
 import LabelTitleText from "@/components/shared/LabelTitleText"
+
 import Plan from "./Plan"
-import { CheckIcon } from "@heroicons/react/outline"
 
-const pricing = {
-  tiers: [
-    {
-      title: "Freelancer",
-      price: 24,
-      frequency: "/month",
-      description: "The essentials to provide your best work for clients.",
-      features: [
-        "5 products",
-        "Up to 1,000 subscribers",
-        "Basic analytics",
-        "48-hour support response time",
-      ],
-      cta: "Monthly billing",
-      mostPopular: false,
-    },
-    {
-      title: "Startup",
-      price: 32,
-      frequency: "/month",
-      description: "A plan that scales with your rapidly growing business.",
-      features: [
-        "25 products",
-        "Up to 10,000 subscribers",
-        "Advanced analytics",
-        "24-hour support response time",
-        "Marketing automations",
-      ],
-      cta: "Monthly billing",
-      mostPopular: true,
-    },
-    {
-      title: "Enterprise",
-      price: 48,
-      frequency: "/month",
-      description: "Dedicated support and infrastructure for your company.",
-      features: [
-        "Unlimited products",
-        "Unlimited subscribers",
-        "Advanced analytics",
-        "1-hour, dedicated support response time",
-        "Marketing automations",
-        "Custom integrations",
-      ],
-      cta: "Monthly billing",
-      mostPopular: false,
-    },
-  ],
-}
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ")
-}
 const Pricing = ({ label, title, text, plans }) => {
   const labelTitleText = { label, title, text }
 
   return (
     <section>
-      <div className="max-w-7xl mx-auto pt-8 px-4 sm:px-6 lg:px-8">
+      <div className="px-4 pt-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <LabelTitleText {...labelTitleText} />
 
         <div className="mt-24 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-8">
