742dca10b781efe32a9073de58982c954ddfff0c
Eric Ahmann <eric@ahmann.me>
2021-11-05 15:17:19 -0500

    test og api

---
+++ nextjs12/src/pages/api/og.tsx
@@ -0,0 +1,16 @@
+import { withOGImage } from "next-api-og-image"
+
+export default withOGImage({
+  template: {
+    html: ({ myQueryParam }) => `
+      <html>
+        <head>
+          <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        </head>
+        <body>
+          <h1 class="flex flex-row text-6xl text-blue-300">${myQueryParam}<span>💻</span></h1>
+        </body>
+      </html>
+    `,
+  },
+})
